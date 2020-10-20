c32f6dfe7e4d297f2b417161539b5cb665afdd5d
AndreyHub09 <70836292+AndreyHub09@users.noreply.github.com>
2020-10-20 23:31:48 +0300

    feat: morse decoding

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let exprConverted = [];
+
+    var codedCharacters = expr.split('**********');
+    for (var i = 0; i < codedCharacters.length; i++) {
+        var startPosition = 0;
+        while (startPosition < codedCharacters[i].length) {
+            var codedCharacter = codedCharacters[i].substring(startPosition, startPosition+10);
+            
+            var morseCode = '';
+            var startMorsePosition = 0;
+            while(startMorsePosition < codedCharacter.length){
+                var codedMorseCharacter = codedCharacter.substring(startMorsePosition, startMorsePosition+2);
+                if(codedMorseCharacter === '10'){
+                    morseCode += '.';
+                }
+                if(codedMorseCharacter === '11'){
+                    morseCode += '-';
+                }
+                startMorsePosition += 2;
+            }
+            exprConverted.push(MORSE_TABLE[morseCode]);
+            startPosition += 10;
+        }
+        if(i !== codedCharacters.length - 1){
+            exprConverted.push(' ');
+        }
+    }
+
+    return exprConverted.join('');
 }
 
 module.exports = {
